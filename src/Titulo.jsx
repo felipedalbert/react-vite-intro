d0a78e0a2e23be63cc4da535826fbd4fcf2c2d18
Felipe Dalbert <felipeoficial195@gmail.com>
2023-07-18 23:37:50 -0300

    useState no react

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
+
 function Titulo(props) {
+
+    const [texto, setTexto] = useState("Um texto do estado inicial")
+    const [titulo, setTitulo] = useState(`Olá, sou ${props.nome ? props.nome : "ninguém"}`)
 
     const urlImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzrtaXGkXhkLIV4Du-Lg0JPJ5I84RQb8RvIA&usqp=CAU"
 
     return (
 
         <div>
-            <h1 style={{color: props.cor}}>Olá, sou {props.nome ? props.nome : "ninguém"}</h1>
+            <h1 style={{color: props.cor}}>{titulo}</h1>
+            <input style={{display: "block"}} value={titulo} onChange={(e) =>{setTitulo(e.target.value)}} />
             <img width={250} src={urlImg} />
             {props.paragrafo &&
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero corporis amet quasi dolorum veniam nesciunt sit, officia eos fugit, tenetur autem dolores! Eaque blanditiis nobis ad labore facilis praesentium alias?</p> 
+                <p>{texto}</p> 
             }
             
+            <button onClick={() => {setTexto(prompt("escreva algo para mudar o parágrafo"))}}>Mudar</button>
         </div >  
     )
 }
