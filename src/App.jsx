3a00d15b69f1d42e4a3b8d7bf71ca04446dfefd7
Felipe Dalbert <felipeoficial195@gmail.com>
2023-07-18 23:12:33 -0300

    components props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Titulo from "./Titulo"
 function App(){
   return (
     <div>
-      <Titulo />
-      <Titulo />
+      <Titulo cor="blue" paragrafo={true} nome="Felipe" />
+      <Titulo cor="green" paragrafo={true} nome="João"/>
+      <Titulo cor="red" paragrafo={true} nome="Ana"/>
+      <Titulo cor="violet" paragrafo={false}/>
     </div>
     
   )
--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,14 +1,16 @@
-function Titulo() {
+function Titulo(props) {
 
-    let nome = "Felipe Dalbert"
     const urlImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzrtaXGkXhkLIV4Du-Lg0JPJ5I84RQb8RvIA&usqp=CAU"
 
     return (
 
         <div>
-            <h1>Olá, sou {nome}</h1>
+            <h1 style={{color: props.cor}}>Olá, sou {props.nome ? props.nome : "ninguém"}</h1>
             <img width={250} src={urlImg} />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero corporis amet quasi dolorum veniam nesciunt sit, officia eos fugit, tenetur autem dolores! Eaque blanditiis nobis ad labore facilis praesentium alias?</p> 
+            {props.paragrafo &&
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero corporis amet quasi dolorum veniam nesciunt sit, officia eos fugit, tenetur autem dolores! Eaque blanditiis nobis ad labore facilis praesentium alias?</p> 
+            }
+            
         </div >  
     )
 }
